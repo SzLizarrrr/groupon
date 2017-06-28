8a69d482872e8b7bddac205b1a4161deb8f6eb47
SzLizarrrr <yf8189@gmail.com>
2017-06-28 01:10:59 -0700

    init

---
+++ Admin/Controller/src/main/java/com/szlizarrrr/GrouponAdminControllerApplication.java
@@ -0,0 +1,12 @@
+package com.szlizarrrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouponAdminControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrouponAdminControllerApplication.class, args);
+	}
+}
---
+++ Service/src/main/java/com/szlizarrrr/GrouponServiceApplication.java
@@ -0,0 +1,12 @@
+package com.szlizarrrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouponServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrouponServiceApplication.class, args);
+	}
+}
